1d9579683149675be53bb699bee4c15c92550f8f
poojadonawadi <138244294+poojadonawadi@users.noreply.github.com>
2024-12-30 18:02:04 +0530

    [Pooja] added: write operation

---
+++ FileWriteOperation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileInputOutput_Practice
+{
+    internal class FileWriteOperation
+    {
+        public static void FileWrite()
+        {
+            string path = @"C:\Users\ADMIN\Desktop\PoojaFile\filemove.txt";
+            string contents = "This is the file write operation";
+           
+            if(File.Exists(path))
+            {
+                //It overrides the existing content in the perticular file
+                File.WriteAllText(path, contents);
+                Console.WriteLine("The content is added to the file successfully");
+            }
+            else
+            {
+                File.Create(path);
+                Console.WriteLine("The file is created and added the content");
+            }
+        }
+    }
+}
