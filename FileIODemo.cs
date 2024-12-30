c71ad78a6ebf4ffff7c676ec229c86003159e646
poojadonawadi <138244294+poojadonawadi@users.noreply.github.com>
2024-12-30 17:51:36 +0530

    [Pooja] added: Demo of file handling

---
+++ FileIODemo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileInputOutput_Practice
+{
+    internal class FileIODemo
+    {
+        public static void FileExistance()
+        {
+            string path = @"C:\Users\ADMIN\source\repos\FileInputOutput Practice\FileInputOutput Practice\Demo.txt";
+            bool result = File.Exists(path);
+            if (result)
+            {
+                Console.WriteLine("File exists in the given path");
+            }
+            else
+            {
+                File.Create(path);
+                Console.WriteLine("File is not present path is created");
+            }
+        }
+        public static void CSVFile()
+        {
+            String path = @"C:\Users\ADMIN\source\repos\FileInputOutput Practice\FileInputOutput Practice\Demo.csv";
+            if (File.Exists(path))
+            {
+                Console.WriteLine("The file exists");
+            }
+            else
+            {
+                File.Create(path);
+                Console.WriteLine("The does not exists it is created");
+            }
+        }
+        
+    }
+}
